d0cfac21df39514f60fad3b97f2d5c32eb8c79b4
JonathanTR <Jonathan.d.reilly@gmail.com>
2013-11-08 21:24:59 -0800

    more validations, topic is unique scoped to user

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -3,5 +3,5 @@ require 'spec_helper'
 describe Topic do
   it { should belong_to(:user) }
   it { should validate_presence_of(:name) }
-  it { should validate_uniqueness_of(:name) }
+  it { should validate_uniqueness_of(:name).scoped_to(:user_id) }
 end
