ab5c6786521209e20728c1502b18e38a90a1f94e
Apprentice <apprentice@dbc-19.local>
2013-11-10 19:02:56 -0800

    APS & DB: pressing add snippet will print selected text of multiple divs to the console

--- app/assets/javascripts/textSelection.js
+++ app/assets/javascripts/textSelection.js
@@ -1,33 +1,24 @@
 var textSelection = {
 
   init: function(){
-    $('')
+    $('.add-snippet').on('click', textSelection.captureTextSelection);
+  },
+
+  captureTextSelection: function(e){
+    e.preventDefault();
+    console.log(textSelection.getTextOfSelectedDivs(textSelection.cloneSelectedDivs()))
+  },
+
+  cloneSelectedDivs: function(){
+    return window.getSelection().getRangeAt(0).cloneContents().children;
+  },
+
+  getTextOfSelectedDivs: function(selectedDivs){
+    return Array.prototype.slice.call(selectedDivs).map(function(div){return div.innerText}).join(' ')
   }
+
 }
+
 $(document).ready(function(){
   textSelection.init();
 })
-
-
-// var WorkSpace = function(){
-//   this.addSnippet;
-// };
-
-// WorkSpace.prototype.addSnippet = function(){
-//   var selectionClone = window.getSelection().getRangeAt(0).cloneContents().children;
-//   Array.prototype.slice.call(.getRangeAt(0).cloneContents().children).map(function(div){return div.innerText}).join(' ')
-// };
-
-// var Snippet = function( text ){
-//   this.text = text;
-// };
-
-// Snippet.prototype.parseText = function(){
-//   Array.prototype.slice.call(this.text).map(function(div){
-//     return div.innerText
-//   }.join(' '))
-// }
-
-// $(function() {
-//   new WorkSpace();
-// })
