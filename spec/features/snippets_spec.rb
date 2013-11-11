0d12326d0582a958e3e18cb5a42f087a87005662
JonathanTR <Jonathan.d.reilly@gmail.com>
2013-11-10 20:33:33 -0800

    JTR/BDOG: Feature test for deleting a snippet, added and styled delete button for snippet, all tests passing

--- spec/features/snippets_spec.rb
+++ spec/features/snippets_spec.rb
@@ -1,1 +1,12 @@
 require 'spec_helper'
+include UserHelper
+
+feature 'Snippets' do
+
+  let!(:user) { FactoryGirl.create(:user_with_topics_with_snippets) }
+
+  scenario 'User can see a delete button for a snippet', js: true do
+    sign_in_as(user)
+    click_on(user.topics.first.name)
+    page.has_selector?(:link_or_button, "Delete")
+  end
