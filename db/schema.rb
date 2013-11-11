264f1bb369974e752fa7da2501992aeb3f4b1c38
Alek Sharma <aleksharma12@gmail.com>
2013-11-11 08:31:51 -0800

    Update schema.rb
    Don't worry about it, still fixing master.

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131111054009) do
+ActiveRecord::Schema.define(:version => 20131110025542) do
 
   create_table "snippets", :force => true do |t|
     t.text     "content"
     t.integer  "topic_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.integer  "user_id"
   end
 
   create_table "topics", :force => true do |t|
