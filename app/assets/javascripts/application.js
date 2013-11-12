3ad82460c9927c270ac20dd888ba842cae208693
Apprentice <apprentice@dbc-19.local>
2013-11-12 09:34:48 -0800

    WIP: we made that shit work by creating a new sources layout.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require ../../../vendor/assets/javascripts/compatibility
 //= require ../../../vendor/assets/javascripts/pdf.worker
 //= require ../../../vendor/assets/javascripts/pdf
+//= require viewer_crap
 //= require_tree .
 //= require bootstrap
 
