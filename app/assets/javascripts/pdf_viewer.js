a6b5268a3fbda4fd3db10ae1ff970e1f9872c444
Apprentice <apprentice@dbc-19.local>
2013-11-12 09:46:26 -0800

    added pdf_viewer(js,css) to isolate the sources layout js and css requirements.

---
+++ app/assets/javascripts/pdf_viewer.js
@@ -0,0 +1,7 @@
+//= require jquery
+//= require ../../../vendor/assets/javascripts/compatibility
+//= require ../../../vendor/assets/javascripts/pdf.worker
+//= require ../../../vendor/assets/javascripts/pdf
+//= require viewer_crap
+
+PDFJS.workerSrc = '/assets/pdf.worker.js';
