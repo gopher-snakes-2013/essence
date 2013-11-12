a397c631ace019d2d6bb819918f879d3eb52b23b
Apprentice <apprentice@dbc-19.local>
2013-11-11 17:14:36 -0800

    WIP: Shadi: partial integration of PDF.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require ../../../vendor/assets/javascripts/compatibility
+//= require ../../../vendor/assets/javascripts/pdf.worker
+//= require ../../../vendor/assets/javascripts/pdf
 //= require_tree .
 //= require bootstrap
+
+PDFJS.workerSrc = '/assets/pdf.worker.js';
