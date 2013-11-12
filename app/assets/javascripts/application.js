22e53c1fbb03320b233360678123784282077966
Apprentice <apprentice@dbc-19.local>
2013-11-12 10:03:32 -0800

    move pdf related javascript and css files under pdf namespace and only require directories in both pdf_viewer(js,css) and application(js,css)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,11 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require ../../../vendor/assets/javascripts/compatibility
-//= require ../../../vendor/assets/javascripts/pdf.worker
-//= require ../../../vendor/assets/javascripts/pdf
-//= require viewer_crap
-//= require_tree .
 //= require bootstrap
+//= require_directory .
 
-PDFJS.workerSrc = '/assets/pdf.worker.js';
