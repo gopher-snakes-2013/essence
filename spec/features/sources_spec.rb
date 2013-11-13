cf6cb0055c71e6ae45eef0a30ea197916455eaec
JonathanTR <Jonathan.d.reilly@gmail.com>
2013-11-13 13:53:33 -0800

    Fixed delete topic styling/animation, added model and some feature tests for source, fixed delete snippet js

--- spec/features/sources_spec.rb
+++ spec/features/sources_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 include UserHelper
+include SourceHelper
 
 feature 'sources library' do
   before(:each) do
@@ -12,15 +13,23 @@ feature 'sources library' do
     end
 
     it "shows a link to a source", js: true do
+      create_sample_source
       click_on('Library')
-      page.should have_link("test doc", href: "/sources/1")
+      page.should have_link("Sample", href: "/sources/1")
     end
 
     # this test is checking for a html rendered pdf, we have it hardcoded right now, **** change it later *****
     it "can click on a source to view it", js: true do
+      create_sample_source
       click_on('Library')
-      click_on('test doc')
+      click_on('Sample')
       expect(current_path).to eq('/sources/1')
+    end
+
+    xit "can create a new source in their library" do
+      click_on('Library')
+
+      page.should have_button("Add PDF")
     end
   end
 end
