d91596b76a94ccd244b33b0c00e86f3afcd5f067
Apprentice <apprentice@dbc-19.local>
2013-11-13 21:26:35 -0800

    APS & JTR: snippets refactoring complete

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,21 +1,13 @@
 class SnippetsController < ApplicationController
-
-  def index
-  end
-
-  def new
-  end
-
   def create
-    topic_id = params[:snippet][:topic_id]
     snippet = Snippet.new
     snippet.content = params[:snippet][:content]
     snippet.user_id = current_user.id || params[:snippet][:user_id]
-    snippet.topic_id = topic_id
+    snippet.topic_id = params[:snippet][:topic_id]
     if snippet.save
       render :nothing => true
     else
-      puts "I shouldn't fail silently"
+      # puts "I shouldn't fail silently"
       render :nothing => true
     end
   end
@@ -25,5 +17,4 @@ class SnippetsController < ApplicationController
     @snippet.destroy
     redirect_to :back
   end
-
 end
--- spec/models/snippet_spec.rb
+++ spec/models/snippet_spec.rb
@@ -1,18 +1,25 @@
 require 'spec_helper'
 
 describe Snippet do
+  let(:snippet) { FactoryGirl.create(:topic_with_snippets).snippets.first }
+
   it {should belong_to(:topic)}
   it {should belong_to(:user)}
   it {should belong_to(:source)}
-
   it {should validate_presence_of(:content)}
   it {should ensure_length_of(:content).is_at_most(255)}
 
   describe "#unlink!" do
     it "removes the topic_id from the snippet" do
-      snippet = FactoryGirl.create(:topic_with_snippets).snippets.first
       snippet.unlink!
       expect(snippet.topic_id).to be(nil)
     end
   end
+
+  describe "#unaffiliated?" do
+    it "returns true if topic_id is nil" do
+      expect(snippet.unaffiliated?).to eq(false)
+    end
+  end
+
 end
