00ff177ab38320a591ecdc23147df8ba311991e7
Apprentice <apprentice@dbc-19.local>
2013-11-14 10:55:49 -0800

    APS: add button to display new topic form removed

--- app/assets/javascripts/topic.js
+++ app/assets/javascripts/topic.js
@@ -8,32 +8,24 @@ var TopicHandler = {
     }
   },
 
-  toggleNewTopicForm: function(){
-    var $form = $(".new-topic-form")
-    if($form.hasClass('hidden')){
-      $form.fadeIn(500);
-      $form.removeClass('hidden')
-    } else {
-      $form.fadeOut(500);
-      $form.addClass('hidden')
-    }
-    $(".new-topic-form").find("input[type=text], textarea").val("");
-  },
-
   displayErrors: function(e, response){
     TopicHandler.toggleNewTopicForm();
     $("#new-topic-errors").html(response.responseJSON.errors[0]);
   },
 
-  clearErrorsDiv: function() {
-    $("#new-topic-errors").html("");
-  },
-
   prependTopic: function(e, response){
     var $new_topic = $(response.responseJSON)
     $(".topic-list").prepend($new_topic);
-    TopicHandler.toggleNewTopicForm();
+    TopicHandler.clearTopicForm();
     TopicHandler.clearErrorsDiv();
+  },
+
+  clearTopicForm: function(){
+    $(".new-topic-form").find("input[type=text], textarea").val("");
+  },
+
+  clearErrorsDiv: function(){
+    $("#new-topic-errors").html("");
   },
 
   removeTopicOnDelete: function(){
@@ -48,9 +40,7 @@ var TopicHandler = {
   }
 }
 
-
 $(document).ready(function(){
-  $("#add-new-topic").on('click', TopicHandler.toggleNewTopicForm);
   $(".new-topic-form form").on('ajax:complete', TopicHandler.init)
   TopicHandler.removeTopicOnDelete();
 })
