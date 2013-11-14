5845068820a6758c185cf864b76aadcab261855e
JonathanTR <Jonathan.d.reilly@gmail.com>
2013-11-13 22:46:45 -0800

    JTR: reorganize Gemfile, no more overlapping groups

--- config/application.rb
+++ config/application.rb
@@ -66,6 +66,6 @@ module Essence
     config.assets.version = '1.0'
 
     # Disable asset precompile for Heroku deployment
-    config.assets.initialize_on_precompile = true
+    # config.assets.initialize_on_precompile = false
   end
 end
