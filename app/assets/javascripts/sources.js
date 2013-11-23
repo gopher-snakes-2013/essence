a300d0da654ff964d2f0d54a99a434454651208f
Apprentice <mail@danbender.net>
2013-11-22 20:25:51 -0800

    bran/dan: - user is asked to confirm deletion of source - unified delete animation for sources, topics, and snippets

--- app/assets/javascripts/sources.js
+++ app/assets/javascripts/sources.js
@@ -7,6 +7,18 @@ var SourcesHandler = {
     }
   },
 
+  showSourceDeleteButton: function() {
+    $('.list_of_sources').on('mouseenter', 'img', function(e){
+      $(this).closest('li').find('.delete-button').show();
+    })
+  },
+
+  hideSourceDeleteButton: function() {
+    $('.list_of_sources').on('mouseleave', '.source', function(e){
+      $(this).find('.delete-button').hide();
+    })
+  },
+
   appendNewSource: function(response) {
     $(".show_pdf_library ul").append(response.responseJSON)
     $(".add_pdf form #source_title").val("")
@@ -19,7 +31,8 @@ var SourcesHandler = {
 
   deleteSource: function() {
   $(".show_pdf_library .list_of_sources").on('ajax:success', '.source .button_to', function(){
-      $(this).closest('li').fadeOut(175, function(){this.remove()});
+      $(this).closest('li').fadeOut(175, function(){
+        this.remove()});
     })
   },
 }
@@ -28,5 +41,7 @@ $(document).ready(function() {
   $(".addSourceUrl").on('ajax:complete', function(e, response) {
     SourcesHandler.init(e, response);
   });
+  SourcesHandler.showSourceDeleteButton();
+  SourcesHandler.hideSourceDeleteButton();
   SourcesHandler.deleteSource();
 })
